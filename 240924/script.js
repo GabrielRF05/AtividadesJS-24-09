64bd55df0cc631fd01b11cddd5c5e52cd4076b4e
GabrielRF05 <gabrieldosreis512@gmail.com>
2024-09-24 15:39:27 -0300

    Add files via upload

---
+++ 240924/script.js
@@ -0,0 +1,5 @@
+let num = parseFloat(prompt("Insira quantos elefantes tem no ambiente"))
+
+for(let i = 0; i < num; i++){
+    console.log(`${i} elefante incomoda muita gente, ${i+1} incomodam muito mais`)
+}
